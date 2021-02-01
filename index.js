1139a8eb028ebc1238e02d13752e09ab4d72b735
Shafeek <mohammed.shafeek@financial-training.com.sg>
2021-02-01 11:12:48 +0800

    Test success

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.get("/", function (req, res) {
     return res.send("This is the sample project to test CI/CD");
 });
 
+
 // listen to port 7000 by default
 app.listen(process.env.PORT || 7000, () => {
     console.log("Server is running");
